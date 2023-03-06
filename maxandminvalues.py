cf2c9dd5785838cd5898eaa22252d7a4ecbbc04d
Patryk Rajtarowski <p.rajtarowski@gmail.com>
2023-03-06 10:07:04 +0100

    min and max values got by 2 way of func

--- maxandminvalues.py
+++ maxandminvalues.py
@@ -2,7 +2,6 @@ import numpy as np
 from random import random, randint
 
 
-
 #max values from array
 value=np.random.uniform(-1000,1020,500)
 
@@ -13,6 +12,22 @@ def max_main(array):
             max=array[i]
     return max
 
-print(max(value))
+
+
+def min_main(array):
+    min= array[0]
+    for i in range(1,len(array-1)):
+        if array[i]<min:
+            min=array[i]
+    return min
+
+
+
+
 max_sys=value.max()
+print(max(value))
 print(max_sys)
+
+min_sys=value.min()
+print(min(value))
+print(min_sys)
