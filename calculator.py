ae72ef39c96c95a6ff1ff73002321e801fe8f7da
PatrykRajtarowski <you@example.com>
2023-03-19 10:57:31 +0100

    changed varsion

--- calculator.py
+++ calculator.py
@@ -10,78 +10,101 @@ window.title("CALCULATOR")
 window.geometry('630x850')
 window.configure(background='#001a00')
 
-calculation=''
+
+saved_values=0
+saved_operator=0
 
 text_input=StringVar()
 
-
 def clear_func():
-    global calculation
-    calculation=''
-    display.delete(1.0,'end')
-
-
-def evaluate_calculation():
-    global calculation
-    try:
-        calculation=str(eval(calculation))
-        display.delete(1.0,'end')
-        display.insert(1.0,calculation)
-        print (calculation)
-    except:
-        clear_func()
-        display.insert(1.0,'ERROR')
-
-
-def  add_to_calculator(symbol):
-    global calculation
-    calculation+= str(symbol)
-    display.delete(1.0,'end')
-    display.insert(1.0,calculation)
-   
+    global saved_values
+    saved_values=''
+    display.delete(0, tk.END)
 
 def backspace():
-    global calculation
-    global calculation
-    calculation= calculation[0:len(calculation)-1]
-    display.delete(1.0,'end')
-    display.insert(1.0,calculation)
+    current_value = display.get()
+    display.delete(len(current_value)-1,tk.END)
     
-    
+def operator_pressed(operator):
+    global saved_number, saved_operator
+
+    # odczytanie wartości z pola tekstowego
+    current_value = display.get()
+
+    if saved_operator:
+        # podmiana operatora bez wykonywania obliczenia
+        saved_operator = operator
+    else:
+        # zapisanie zapamiętanej liczby i operatora
+        saved_number = float(current_value)
+        saved_operator = operator
+
+        # wyczyszczenie pola tekstowego
+        display.delete(0, tk.END)
+
+# funkcja do wykonania obliczenia po wciśnięciu przycisku =
+def perform_calculation():
+    global saved_number, saved_operator
+
+    # odczytanie wartości z pola tekstowego
+    current_value = display.get()
+    new_number = float(current_value)
+
+    # wykonanie obliczenia i wyświetlenie wyniku
+    if saved_operator == '+':
+        result = saved_number + new_number
+    elif saved_operator == '-':
+        result = saved_number - new_number
+    elif saved_operator == '*':
+        result = saved_number * new_number
+    elif saved_operator == '/':
+        result = saved_number / new_number
+    else:
+        result = float(new_number)
+    display.delete(0, tk.END)
+    display.insert(0, str(result))
+
+    # zresetowanie zmiennych globalnych
+    saved_number = result
+    saved_operator = ''
 
 
-display=Text(window,width=16,height=2,border=3,font=('Arial',24),background='#00cc00')
+
+
+
+
+display=Entry(window,width=16,border=3,font=('Arial',24),background='#00cc00')
 display.grid(row=0,column=0,columnspan=7,pady=10,padx=10,sticky ='swse')
 
-b7 = Button(window, text='7',command=lambda:add_to_calculator(7),font=('Arial', 45))
+b7 = Button(window, text='7',command=lambda:display.insert(tk.END, '7'),font=('Arial', 45))
 b7.grid(row=2, column=0, padx=10, pady=10)
-b8 = Button(window, text='8',command=lambda:add_to_calculator(8), font=('Arial', 45))
+b8 = Button(window, text='8',command=lambda:display.insert(tk.END, '8'), font=('Arial', 45))
 b8.grid(row=2, column=1, padx=10, pady=10)
-b9 = Button(window, text='9',command=lambda:add_to_calculator(9), font=('Arial', 45))
+b9 = Button(window, text='9',command=lambda:display.insert(tk.END, '9'), font=('Arial', 45))
 b9.grid(row=2, column=2, padx=10, pady=10)
-b4 = Button(window, text='4',command=lambda:add_to_calculator(4), font=('Arial', 45))
+b4 = Button(window, text='4',command=lambda:display.insert(tk.END, '4'), font=('Arial', 45))
 b4.grid(row=3, column=0, padx=10, pady=10)
-b5 = Button(window, text='5',command=lambda:add_to_calculator(5), font=('Arial', 45))
+b5 = Button(window, text='5',command=lambda:display.insert(tk.END, '5'), font=('Arial', 45))
 b5.grid(row=3, column=1, padx=10, pady=10)
-b6 = Button(window, text='6',command=lambda:add_to_calculator(6), font=('Arial', 45))
+b6 = Button(window, text='6',command=lambda:display.insert(tk.END, '6'), font=('Arial', 45))
 b6.grid(row=3, column=2, padx=10, pady=10)
-b1 = Button(window, text='1',command=lambda:add_to_calculator(1), font=('Arial', 45))
+b1 = Button(window, text='1',command=lambda:display.insert(tk.END, '1'), font=('Arial', 45))
 b1.grid(row=4, column=0, padx=10, pady=10)
-b2 = Button(window, text='2',command=lambda:add_to_calculator(2), font=('Arial', 45))
+b2 = Button(window, text='2',command=lambda:display.insert(tk.END, '2'), font=('Arial', 45))
 b2.grid(row=4, column=1, padx=10, pady=10)
-b3 = Button(window, text='3',command=lambda:add_to_calculator(3), font=('Arial', 45))
+b3 = Button(window, text='3',command=lambda:display.insert(tk.END, '3'), font=('Arial', 45))
 b3.grid(row=4, column=2, padx=10, pady=10)
-b10 = Button(window, text='-',command=lambda:add_to_calculator('-'), font=('Arial', 45),background="#b3b3b3")
+b10 = Button(window, text='-',command=lambda:operator_pressed('-'), font=('Arial', 45),background="#b3b3b3")
 b10.grid(row=4, column=3, padx=10, pady=10)
-b11 = Button(window, text='/',command=lambda:add_to_calculator('/'), font=('Arial', 45),width=2,background="#b3b3b3")
+b11 = Button(window, text='/',command=lambda:operator_pressed('/'), font=('Arial', 45),width=2,background="#b3b3b3")
 b11.grid(row=3, column=3, padx=10, pady=10)
-b12 = Button(window, text='*',command=lambda:add_to_calculator('*'), font=('Arial', 45),width=2,background="#b3b3b3")
+b12 = Button(window, text='*',command=lambda:operator_pressed('*'), font=('Arial', 45),width=2,background="#b3b3b3")
 b12.grid(row=2, column=3, padx=10, pady=10)
-b13 = Button(window, text='+', command=lambda:add_to_calculator('+'),font=('Arial', 45),width=2,background="#b3b3b3")
+b13 = Button(window, text='+', command=lambda:operator_pressed('+'),font=('Arial', 45),width=2,background="#b3b3b3")
 b13.grid(row=5, column=3, padx=10, pady=10)
-b14 = Button(window, text='=', command=evaluate_calculation,font=('Arial', 45),width=2,background="#b3b3b3")
+b14 = Button(window, text='=', command=perform_calculation,font=('Arial', 45),width=2,background="#b3b3b3")
 b14.grid(row=6, column=3, padx=10, pady=10)
-b15 = Button(window, text='.',command=lambda:add_to_calculator('.'), height=2,font=('Arial',25),width=10,background="#b3b3b3")
+b15 = Button(window, text='.',command=lambda:display.insert(tk.END, '.'), height=2,font=('Arial',25),width=10,background="#b3b3b3")
 b15.grid(row=6, column=0, padx=10, pady=10,columnspan=3, rowspan=1, sticky ='swse')
 
 b16= Button(window, text='0',height=2, font=('Arial', 27),width=10)
